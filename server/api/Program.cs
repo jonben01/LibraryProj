2d41747248a8c86c949f1ceb8f0008ae4bb77171
Jonas <jonasbendorff@gmail.com>
2025-09-19 17:56:17 +0200

    scaffolding + connection string

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,37 @@
+using DotNetEnv;
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("CONN_STR");
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(connectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+{
+    
+    var book = new Book()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Title = "Jdoas",
+        Pages = 1,
+        Createdat = DateTime.UtcNow,
+        Authors =  null
+    };
+    dbContext.Books.Add(book);
+    dbContext.SaveChanges();
+    var objects = dbContext.Books.ToList();
+    return objects;
+
+});
 
 app.Run();
