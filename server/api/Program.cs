05cff24daf4be4b897381b40fa2528b539356f73
Jonas <jonasbendorff@gmail.com>
2025-09-22 14:35:09 +0200

    switching to options pattern, created dockerfile

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -6,32 +6,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Env.Load();
-var connectionString = Environment.GetEnvironmentVariable("CONN_STR");
 
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql(connectionString);
+    conf.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString"));
 });
 
 var app = builder.Build();
 
 app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
 {
-    
-    var book = new Book()
-    {
-        Id = Guid.NewGuid().ToString(),
-        Title = "Jdoas",
-        Pages = 1,
-        Createdat = DateTime.UtcNow,
-        Authors =  null
-    };
-    dbContext.Books.Add(book);
-    dbContext.SaveChanges();
-    var objects = dbContext.Books.ToList();
-    return objects;
-
+    Console.WriteLine("Hello World!");
 });
 
 app.Run();
